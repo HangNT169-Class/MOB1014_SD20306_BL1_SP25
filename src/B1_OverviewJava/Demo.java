e72f09835b1f76452c570a6cd79392dfbfb8a050
HangNT169 <HangNT169@fpt.edu.vn>
2025-01-07 13:59:57 +0700

    Buổi 1: Tạo và chạy helloworld

---
+++ src/B1_OverviewJava/Demo.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package B1_OverviewJava;
+
+/**
+ *
+ * @author hangnt169
+ */
+public class Demo {
+
+    public static void main(String[] args) {
+        System.out.println("Demo2");
+    }
+}
---
+++ src/B1_OverviewJava/Test.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package B1_OverviewJava;
+
+/**
+ *
+ * @author hangnt169
+ */
+public class Test {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // in ra man hinh
+        // C: => printf(")
+        // Java: sout 
+        System.out.print("Helloworld");
+        System.out.println("Test");
+    }
+
+}
